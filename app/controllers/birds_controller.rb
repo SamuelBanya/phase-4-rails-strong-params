b111a00403ed077e11c8043d2952aff14fc366fd
Samuel Banya <sbanya@fastmail.com>
2022-10-21 19:08:04 -0500

    Adding latest changes to assignment

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,15 +1,10 @@
 class BirdsController < ApplicationController
+  wrap_parameters format: []
 
   # GET /birds
   def index
     birds = Bird.all
     render json: birds
-  end
-
-  # POST /birds
-  def create
-    bird = Bird.create(name: params[:name], species: params[:species])
-    render json: bird, status: :created
   end
 
   # GET /birds/:id
@@ -20,6 +15,24 @@ class BirdsController < ApplicationController
     else
       render json: { error: "Bird not found" }, status: :not_found
     end
+  end
+
+  # POST /birds
+  def create
+    # NOTE: This is the previous code without using 'strong params':
+    # bird = Bird.create(name: params[:name], species: params[:species])
+    # render json: bird, status: :created
+    # Using entire 'params' hash:
+    # NOTE: This is the previous version that does NOT use the 'private' method of using 'strong params'
+    # bird = Bird.create(params.permit(:name, :species))
+    bird = Bird.create(bird_params)
+    render json: bird, status: :created
+  end
+
+  private
+
+  def bird_params
+    params.permit(:name, :species)
   end
 
 end
